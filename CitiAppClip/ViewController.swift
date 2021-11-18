99e1b4470fd1b2e7c8835815a4b89e6c6cd5e341
Virender Kumar <viru.dall@gmail.com>
2021-11-18 16:03:38 +0530

    Initial Commit

---
+++ CitiAppClip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CitiAppClip
+//
+//  Created by Virender Dall on 18/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
